4dbd70df4863cb076dc94ee29916d63d852fa21e
Gina Magnussen <ginama@null06.math.ntnu.no>
2018-02-02 14:29:33 +0100

    JAA

--- proj_1/exercise1d.R
+++ proj_1/exercise1d.R
@@ -1,11 +1,16 @@
 # Sample from generic gamma distribution 
-setwd("/home/shomea/m/marcusae/Documents/git/Tma4300-Kode/proj_1")
+#setwd("/home/shomea/m/marcusae/Documents/git/Tma4300-Kode/proj_1")
 source("sampleGamma.R")
 
-alpha <- 10
-beta <- 2
+alpha <- 1
+beta <- 0.2
 n = 1000
 
 xSample <- sampleGamma(alpha, beta, n)
 
-hist(xSample, xlim = range(0:200))
+hist(xSample, xlim = range(0:10))
+
+print(mean(xSample))
+print(var(xSample))
+
+
