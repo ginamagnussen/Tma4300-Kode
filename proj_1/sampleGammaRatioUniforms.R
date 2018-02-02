eba19e45d6e4d36241186892fd7eaea31377d8cd
ginamagnussen <31470965+ginamagnussen@users.noreply.github.com>
2018-02-02 08:46:24 +0100

    Update sampleGammaRatioUniforms.R

--- proj_1/sampleGammaRatioUniforms.R
+++ proj_1/sampleGammaRatioUniforms.R
@@ -1,12 +1,13 @@
 # ---------- Utilities ------------ #
 f_star <- function(x, alpha){
-  return (((alpha-1)/2)*x -exp(x))
+  return (((alpha-1)/2)*x -exp(x)/2)
 }
 
 sampleGammaRatioUniforms <- function(alpha, n) { 
   
   # Determine bounds on a log scale
-  log_a_pluss <- (alpha-1)/2 * log((alpha-1)*exp(-1))
+  #log_a_pluss <- (alpha-1)/2 * log((alpha-1)*exp(-1))
+  log_a_pluss <- (alpha-1)/2 * (log(alpha-1)-1)
   # log_a_pluss <- log(sqrt((alpha-1)^(alpha-1)*exp(1-alpha)))
   #log_a_minus <- -Inf
   log_b_pluss <- (alpha+1)/2 * log((alpha+1)*exp(-1))
@@ -29,15 +30,17 @@ sampleGammaRatioUniforms <- function(alpha, n) {
     # Change of variables
     # x_2 <- log_u_1
     # x_1 <- log_u_2 - log_u_1
-    log_x <- log_u_2 - log_u_1 #log(u1/u2)
+    log_x <- log_u_2 - log_u_1 #log(u2/u1)
     
     # Acceptance
+    print(log_u_1 - f_star(log_x, alpha)); flush.console()
     if (log_u_1 <= f_star(log_x, alpha)) {
       
       xSample[it] <- log_x
       
       # Increment
       it <- it + 1
+      print(log_u_1 - f_star(log_x, alpha)); flush.console()
       count <- count + 1
     }
     else {
@@ -49,4 +52,6 @@ sampleGammaRatioUniforms <- function(alpha, n) {
   
   return (struct)
 }
+
+
 
